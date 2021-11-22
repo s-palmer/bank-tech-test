42f8621cc81a70640568a997b8ec27e1e524116f
Sergei Palmer <zigpalmer@gmail.com>
2021-11-22 16:24:22 +0000

    Added tests and methods for bank account - add money + deduct money. Created transaction class file and spec file

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,9 +1,18 @@
 class BankAccount
+
   def initialize
     @balance = 0
   end
 
   def print_balance
     @balance
+  end
+
+  def add_money(amount)
+    @balance += amount
+  end
+
+  def deduct_money(amount)
+    @balance -= amount
   end
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -2,10 +2,27 @@ require 'bank_account'
 
 describe BankAccount do
 
+  before :each do
+    @my_account = BankAccount.new
+  end
+
   describe '#print_balance' do
     it 'returns the current balance' do
-      my_account = BankAccount.new
-      expect(my_account.print_balance).to eq 0
+      expect(@my_account.print_balance).to eq 0
+    end
+  end
+
+  describe '#add_money' do
+    it 'should add money to the account' do
+      @my_account.add_money(10)
+      expect(@my_account.print_balance).to eq 10
+    end
+
+  describe '#deduct_money' do
+    it 'should remove money from the account' do
+      @my_account.add_money(20)
+      @my_account.deduct_money(10)
+      expect(@my_account.print_balance).to eq 10
     end
   end
 end
