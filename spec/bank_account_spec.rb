86e62348174c5aace8d85e0fa5d061c9932e8326
Sergei Palmer <zigpalmer@gmail.com>
2021-11-22 16:28:14 +0000

    Corrected spec file for missing end

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,28 +1,28 @@
-require 'bank_account'
+require "bank_account"
 
 describe BankAccount do
-
   before :each do
     @my_account = BankAccount.new
   end
 
-  describe '#print_balance' do
-    it 'returns the current balance' do
+  describe "#print_balance" do
+    it "returns the current balance" do
       expect(@my_account.print_balance).to eq 0
     end
   end
 
-  describe '#add_money' do
-    it 'should add money to the account' do
+  describe "#add_money" do
+    it "should add money to the account" do
       @my_account.add_money(10)
       expect(@my_account.print_balance).to eq 10
     end
 
-  describe '#deduct_money' do
-    it 'should remove money from the account' do
-      @my_account.add_money(20)
-      @my_account.deduct_money(10)
-      expect(@my_account.print_balance).to eq 10
+    describe "#deduct_money" do
+      it "should remove money from the account" do
+        @my_account.add_money(20)
+        @my_account.deduct_money(10)
+        expect(@my_account.print_balance).to eq 10
+      end
     end
   end
 end
